d9397a200095b229cb10ed8aa04139de1996f6e2
lorenaolaru <74786624+lorenao15@users.noreply.github.com>
2022-01-27 23:44:19 +0000

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,7 @@ const IndexPage = () => {
       <h1>We have two types of cards!</h1>
       <p>You can hoose between the two from the navigation tab.</p>
       <p>
-        Helloooooooooooooooooo??? Does it wok?This is what the new script does!!!
-        Why don't you start!!!!
+        Helloooooooooooooooooo???
       </p>
     </Layout>
   )
