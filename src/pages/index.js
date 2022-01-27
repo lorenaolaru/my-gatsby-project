bb2a31982b49a1d5aff4168de02a277ae2ec8dd1
lorenaolaru <74786624+lorenao15@users.noreply.github.com>
2022-01-27 23:29:13 +0000

    Test travis

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => {
       <h1>We have two types of cards!</h1>
       <p>You can hoose between the two from the navigation tab.</p>
       <p>
-        Helloooooooooooooooooo
+        Helloooooooooooooooooo??? Does it wok?
       </p>
     </Layout>
   )
